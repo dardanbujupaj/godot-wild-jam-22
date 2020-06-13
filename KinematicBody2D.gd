95645398c92eb22e4227c86e37c7596559c3dfd7
itsBelinda <belinda.kneubuehler@outlook.com>
2020-06-13 17:44:17 +0100

    basic character moves with gravity

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -5,21 +5,30 @@ extends KinematicBody2D
 # var a = 2
 # var b = "text"
 
+export (int) var speed = 100
+onready var gravity = 20*ProjectSettings.get_setting("physics/2d/default_gravity")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	var velocity = Vector2(0, 5)
-	if Input.is_action_pressed("ui_right"):
-		velocity += Vector2(5, 0)
-	if Input.is_action_pressed("ui_left"):
-		velocity += Vector2(-5, 0)
-	if Input.is_action_pressed("ui_up"):
-		velocity += Vector2(0, -10)
-		
-	move_and_slide(velocity)
-	pass
+var velocity = Vector2()
+
+func get_input():
+	velocity = Vector2()
+	if Input.is_action_pressed('ui_right'):
+		velocity.x += 1
+	if Input.is_action_pressed('ui_left'):
+		velocity.x -= 1
+	if Input.is_action_pressed('ui_down'):
+		velocity.y += 1
+	if Input.is_action_pressed('ui_up'):
+		velocity.y -= 1
+	velocity = velocity.normalized() * speed
+
+func _physics_process(delta):
+	get_input()
+	# Vertical movement code. Apply gravity.
+	velocity.y += gravity * delta
+	velocity = move_and_slide_with_snap(velocity, Vector2.DOWN, Vector2.UP) #move_and_slide(velocity)
