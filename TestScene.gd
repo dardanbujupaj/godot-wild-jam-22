0013673880e352421f8e909a2007d9559722de1c
itsBelinda <belinda.kneubuehler@outlook.com>
2020-06-21 19:00:57 +0100

    fix wrong path

--- TestScene.gd
+++ TestScene.gd
@@ -9,12 +9,9 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	
-	
-	
 	for child in $Collectibles.get_children():
-		child.connect("collect" ,$Character/Camera2D/BaguetteCount, "_on_Coin_collect")
-	
-	
+		child.connect("collect" ,$GUI/MarginContainer/BaguetteCount, "_on_Coin_collect")
+
 	pass # Replace with function body.
 
 
